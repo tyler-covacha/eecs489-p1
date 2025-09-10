7af99251dfcd144aecc03e40fdfe9d1ceb3913dc
tyler-covacha <tylercovacha@gmail.com>
2025-09-09 20:26:53 -0400

    derp

--- util/topology.py
+++ util/topology.py
@@ -65,7 +65,7 @@ if __name__ == '__main__':
     # Run network
     net.start()
     h1 = net.get('h1')
-    h1.cmd('ping -c 5 10.0.0.2 > latency.txt')
-    CLI( net )
+    # h1.cmd('ping -c 5 10.0.0.2 > latency.txt')
+    # CLI( net )
     net.stop()
 
